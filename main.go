0d35c64657c0b4901e3bf36aafe0e857892a6f83
Jonathan Gober <gober.jonathan@gmail.com>
2022-01-18 22:24:33 -0500

    finishing for the day

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	fmt.Println("Welcome to", conferenceName, "application")
+	fmt.Println("Get Tickets here to attend")
+
 }
